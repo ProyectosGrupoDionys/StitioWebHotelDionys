fff66a09c830875af89a2fa998f2fa3756e08d70
DiegoSTI <soporteti2@grupodionys.com.pe>
2018-01-30 16:15:27 -0500

    actualizacion banner

---
+++ ScriptMenu.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	$("#abrir_menu").click(function(){
+		$("#menu").animate({"right":"0"});
+	});
+
+	$("#cerrar_menu").click(function(){
+		$("#menu").animate({"right":"-100%"});
+	});
+
+
+
+});
